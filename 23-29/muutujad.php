8c6043cacc61902432090fb9729f0f3548417c02
Renar Suurpere <renar.suurpere@khk.ee>
2020-11-24 11:23:33 +0200

    PHP alused - Ülesanne 1 Muutujad

---
+++ 23-29/muutujad.php
@@ -0,0 +1,20 @@
+<?php
+/* Ülesanne 1 - Muutujad
+renar.Suurpere
+kuupäev: 24.11.2020
+*/
+$eesnimi = 'Renar';
+$perenimi = "Suurpere";
+$nimi = $eesnimi.' '.$perenimi;
+$synniaasta = 1994;
+$tahtkuju = 'kalad';
+
+echo 'Minu nimi on '.$nimi.'<br>'.'Ma olen sündinud '.$synniaasta.' aastal'.'<br>'.$tahtkuju.' tähtkujus.'.'<br>';
+echo '<br>';
+echo "\"It's My Life\" - Dr.Alban<br>";
+echo '<br>';
+echo '<br>';
+
+echo '(\(\ '.'<br>';
+echo '(-.-)'.'<br>';
+echo 'o_(")(")'.'<br>';
